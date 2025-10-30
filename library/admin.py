c5c8d5caf381810e562ea0c37f3ea762c25bf06a
Alex <oleksiymosiychuk@gmail.com>
2025-10-30 23:05:05 +0300

    add models

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (Authors, Category,
+                     Book,
+                     # BookParameters
+                     )
 
-# Register your models here.
+@admin.register(Authors)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "mid_name", "lust_name")
+    list_filter = ("lust_name",)
+    search_fields = ("lust_name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("author",)
+    search_fields = ("name", )
