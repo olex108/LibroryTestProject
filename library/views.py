4fc578731baff923b0896022645f119ff4d61338
Alex <oleksiymosiychuk108@gmail.com>
2025-11-09 23:41:43 +0300

    update forms

--- library/views.py
+++ library/views.py
@@ -6,6 +6,8 @@ from .models import Categories, Authors, Books
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from .forms import AuthorsForm, BooksForm, CategoriesForm
+
 
 class BooksListView(ListView):
     model = Books
@@ -15,10 +17,9 @@ class BooksListView(ListView):
 
 class BookCreateView(CreateView):
     model = Books
-    fields = ["title", "category_pk", "author_pk", "publication_year", "description"]
+    form_class = BooksForm
     template_name = "library/book_form.html"
     success_url = reverse_lazy("library:books_list")
-
 
 
 class BookDetailView(DetailView):
@@ -29,7 +30,7 @@ class BookDetailView(DetailView):
 
 class BookUpdateView(UpdateView):
     model = Books
-    fields = ["title", "category_pk", "author_pk", "publication_year", "description"]
+    form_class = BooksForm
     template_name = "library/book_form.html"
     success_url = reverse_lazy("library:books_list")
 
@@ -40,36 +41,43 @@ class BookDeleteView(DeleteView):
     success_url = reverse_lazy("library:books_list")
 
 
+class AuthorsListView(ListView):
+    model = Authors
+    template_name = "library/authors_list.html"
+    context_object_name = "authors"
+
+
+class AuthorCreateView(CreateView):
+    model = Authors
+    form_class = AuthorsForm
+    template_name = "library/author_form.html"
+    success_url = reverse_lazy("library:authors_list")
+
+class AuthorUpdateView(UpdateView):
+    model = Authors
+    form_class = AuthorsForm
+    template_name = "library/author_form.html"
+    success_url = reverse_lazy("library:authors_list")
+
+
+class AuthorDetailView(DetailView):
+    model = Authors
+    template_name = "library/author_detail.html"
+    context_object_name = "author"
+
+
+class CategoriesListView(ListView):
+    model = Categories
+    template_name = "library/categories_list.html"
+    context_object_name = "categories"
+
+
+class CategoryCreateView(CreateView):
+    model = Categories
+    form_class = CategoriesForm
+    template_name = "library/category_form.html"
+    success_url = reverse_lazy("library:categories_list")
+
 
 def main(request):
     return render(request, 'library/main.html')
-
-
-def categories(request):
-    categories = Categories.objects.all()
-    context = {
-        'categories': categories,
-    }
-    return render(request, 'library/categories.html', context)
-
-
-def authors(request):
-    authors = Authors.objects.all()
-    context = {
-        "authors": authors,
-    }
-    return render(request, "library/example_authors.html", context)
-#
-# def books_list(request):
-#     books = Books.objects.all()
-#     context = {
-#         "books": books,
-#     }
-#     return render(request, "library/books_list.html", context)
-#
-# def book_details(request, book_id):
-#     book = Books.objects.get(id=book_id)
-#     context = {
-#         "book": book,
-#     }
-#     return render(request, "library/book_detail.html", context)
