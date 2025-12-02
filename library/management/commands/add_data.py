b295695759379956f5c5df67a08ed9e3d4fddc2c
Alex <oleksiymosiychuk108@gmail.com>
2025-12-02 21:23:18 +0300

    update project with permissions

--- library/management/commands/add_data.py
+++ library/management/commands/add_data.py
@@ -17,69 +17,6 @@ class Command(BaseCommand):
         Categories.objects.all().delete()
         Books.objects.all().delete()
 
-
-        # author_1 = Authors.objects.get_or_create(
-        #     first_name = "Лев",
-        #     lust_name = "Толстой",
-        #     mid_name = "Николаевич",
-        #     birthday = 1928,
-        #     portrait = None
-        #
-        # )
-        #
-        # category_1 = Categories.objects.get_or_create(
-        #     name = "Роман",
-        #     description = "Большое повествовательное художественное произведение со сложным сюжетом",
-        # )
-        #
-        # books = [
-
-        # ]
-
-        # category_1, _ = Category.objects.get_or_create(
-        #     name='Смартфоны',
-        #     description='Смартфон это телефон, имеющий начинку и функционал почти как у компьютера'
-        # )
-        # category_2, _ = Category.objects.get_or_create(
-        #     name='Смарт-часы',
-        #     description='Типовыми функциями смарт-часов и браслетов являются подсчет количества шагов'
-        # )
-        #
-        # products = [
-        #     {
-        #         "name": 'Samsung Galaxy S25 Ultra',
-        #         "description": 'Samsung Galaxy S25 Ultra - новый взгляд на будущее мобильных технологий',
-        #         "price": 99990,
-        #         "category": category_1,
-        #     },
-        #     {
-        #         "name": 'Samsung Galaxy A56',
-        #         "description": 'Samsung Galaxy A56 – это новый смартфон 2025 года с мощным процессором Exynos 1580',
-        #         "price": 27390,
-        #         "category": category_1,
-        #     },
-        #     {
-        #         "name": 'Samsung Galaxy Watch8',
-        #         "description": 'Смарт-часы Samsung Galaxy Watch 8 44mm — это именно то, что вам нужно!',
-        #         "price": 20990,
-        #         "category": category_2,
-        #     },
-        #     {
-        #         "name": 'Samsung Galaxy Watch8 Classic',
-        #         "description": 'Умные часы Samsung Galaxy Watch8 Classic 46mm — это не просто аксессуар, а мощный инструмент',
-        #         "price": 25990,
-        #         "category": category_2,
-        #     },
-        #
-        # ]
-
-        # for product in products:
-        #     product, created = Product.objects.get_or_create(**product)
-        #     if created:
-        #         self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name}'))
-        #     else:
-        #         self.stdout.write(self.style.WARNING(f'Product already exists: {product.name}'))
-
         call_command("loaddata", "data_fixtures/authors_programming.json")
         call_command("loaddata", "data_fixtures/categories_programming.json")
         call_command("loaddata", "data_fixtures/books_programming.json")
--- users/management/commands/createadmin.py
+++ users/management/commands/createadmin.py
@@ -4,18 +4,44 @@ from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        User = get_user_model()
-        user = User.objects.create(
-            email="testadmin@gmail.com",
-            first_name="Admin",
-            last_name="Admin",
-        )
+        try:
+            User = get_user_model()
+            user = User.objects.create(
+                email="testadmin@gmail.com",
+                first_name="Admin",
+                last_name="Admin",
+            )
 
-        user.set_password("1234")
+            user.set_password("1234")
 
-        user.is_staff = True
-        user.is_superuser = True
+            user.is_staff = False
+            user.is_superuser = False
 
-        user.save()
+            user.save()
 
-        self.stdout.write(self.style.SUCCESS(f"Successfully created user {user.email}!"))
+            self.stdout.write(self.style.SUCCESS(f"Successfully created user {user.email}!"))
+        except Exception as e:
+            print(e)
+
+        try:
+            User = get_user_model()
+            user = User.objects.create(
+                email="testadminsup@gmail.com",
+                first_name="Admin_sup",
+                last_name="Admin_sup",
+            )
+
+            user.set_password("1234")
+
+            user.is_staff = True
+            user.is_superuser = True
+
+            user.save()
+
+            self.stdout.write(self.style.SUCCESS(f"Successfully created user {user.email}!"))
+
+        except Exception as e:
+            print(e)
+
+
+
